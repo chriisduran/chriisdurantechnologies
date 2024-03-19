3412037545e840d88d67a7d79b1f483749050023
chriisduran <chrisdf2019@gmail.com>
2024-03-18 22:39:17 -0600

    :sparkles: config

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>chriisdurantechnologies</h1>
       </div>
-      <h1>chriisdurantechnologies</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
